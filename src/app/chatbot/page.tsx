b37cb5942b4c24e661b596e5004943923358222a
M00nNight <khanzamil267@gmail.com>
2024-06-25 14:34:00 +0530

    Feat:Added openai and realtime chat with bot && more

---
+++ src/app/chatbot/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import AiChatBot from "@/components/chatbot";
+import React from "react";
+
+type Props = {};
+
+const Chatbot = (props: Props) => {
+  return <AiChatBot />;
+};
+
+export default Chatbot;
